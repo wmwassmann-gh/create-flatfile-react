03dbd7ee94ced28ec122540a5e37579d116eb801
Ashley Mulligan <ashleygmulligan@gmail.com>
2023-06-27 17:00:42 -0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { ISpace, makeTheme, useSpace } from "@flatfile/react";
+import { config } from "./config";
+import { listener } from "./listener";
+
+const spaceProps: ISpace = {
+  name: "Embedded Space",
+  // to test locally add your local vars here
+  publishableKey: "pk_FKfafGxzQRg2tuwpAzz7oO3xOCHapTJo",
+  environmentId: "us_env_fTVuSWQg",
+  workbook: config,
+  themeConfig: makeTheme({ primaryColor: "#546a76", textColor: "#fff" }),
+  sidebarConfig: {
+    showDataChecklist: false,
+    showSidebar: false,
+  },
+  listener: listener,
+};
+
+export default function App() {
+  const [showSpace, setShowSpace] = useState(false);
+  const space = useSpace({
+    ...spaceProps,
+    closeSpace: {
+      operation: "contacts:submit",
+      onClose: () => setShowSpace(false),
+    },
+  });
+
+  return (
+    <div style={{ padding: "16px" }}>
+      <button
+        onClick={() => {
+          setShowSpace(!showSpace);
+        }}
+      >
+        {showSpace === true ? "Close" : "Open"} space
+      </button>
+      {showSpace && space}
+    </div>
+  );
+}
---
+++ src/listener.ts
@@ -0,0 +1,81 @@
+import { FlatfileListener } from "@flatfile/listener";
+import api, { Flatfile } from "@flatfile/api";
+
+async function submit(jobId: string) {
+  try {
+    await api.jobs.ack(jobId, {
+      info: "I'm starting the job - inside client",
+      progress: 33,
+    });
+  } catch (e) {
+    throw new Error(`Error acknowledging jobId: ${jobId} ${e}`);
+  }
+
+  // hit your api or webhook here
+  await new Promise((res) => setTimeout(res, 2000));
+
+  try {
+    await api.jobs.complete(jobId, {
+      info: "Job's work is done",
+      outcome: { next: { type: "wait" } },
+    });
+  } catch (e) {
+    throw new Error(`Error completing jobId: ${jobId} ${e}`);
+  }
+}
+
+async function joinFields(jobId: string, sheetId: string) {
+  try {
+    await api.jobs.ack(jobId, {
+      info: "I'm starting the joining fields job",
+    });
+  } catch (e) {
+    throw new Error(`Error acknowledging jobId: ${jobId} ${e}`);
+  }
+
+  try {
+    const records = await api.records.get(sheetId);
+    const recordsUpdates = records.data.records?.map((record) => {
+      const fullName = `${record.values["first_name"].value} ${record.values["last_name"].value}`;
+      record.values["full_name"].value = fullName;
+      return record;
+    });
+
+    await api.records.update(sheetId, recordsUpdates as Flatfile.Record_[]);
+  } catch (e) {
+    throw new Error(`Error updating records`);
+  }
+
+  try {
+    await api.jobs.complete(jobId, {
+      info: "Job's work is done",
+    });
+  } catch (e) {
+    throw new Error(`Error completing job: ${jobId}`);
+  }
+}
+
+/**
+ * Example Listener
+ */
+export const listener = FlatfileListener.create((client) => {
+  client.on(
+    "job:ready",
+    // @ts-ignore
+    { payload: { operation: "contacts:join-fields" } },
+    async (event: any) => {
+      const { context } = event;
+      return joinFields(context.jobId, context.sheetId);
+    }
+  );
+
+  client.on(
+    "job:ready",
+    // @ts-ignore
+    { payload: { operation: "contacts:submit" } },
+    async (event: any) => {
+      const { context } = event;
+      return submit(context.jobId);
+    }
+  );
+});
