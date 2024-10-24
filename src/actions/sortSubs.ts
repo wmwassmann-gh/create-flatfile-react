37f873b26b03506c1cc55b9e00e9ac111191cabd
wmwassmann <wmwassmann@hotmail.com>
2024-10-24 15:59:46 -0700

    Adding more pieces to the puzzle.  I like the tacklebox compartmentalization more even if it might take longer.

--- src/actions/sortSubs.ts
+++ src/actions/sortSubs.ts
@@ -1,28 +1,37 @@
-import api from "@flatfile/api";
+// import api from "@flatfile/api";
 // import { FlatfileEvent } from "@flatfile/listener";
 import { ActionSpec } from "../plugins/jobs";
 
+// export const changeCase: ActionSpec = {
 export const sortSubs: ActionSpec = {
   blueprint: {
     operation: "sort-subs",
     mode: "foreground",
     label: "Sort Subscribers",
     description: "This will sort subscribers and non-subscribers."
+    // operation: "change-case",
+    // mode: "foreground",
+    // label: "change-case",
+    // description: "This will change the case of the selected fields."
   },
-  handler: async (event, job) => {
-    const { spaceId, sheetId, workbookId, jobId } = event.context;
-    // possibly more    
-    try { 
-      console.log("Good here");
+  // handler: async (event, job) => {
+  //   const { 
+  //     // spaceId, 
+  //     // sheetId, 
+  //     // workbookId, 
+  //     // jobId 
+  //   } = event.context;
+     
+  //   try { 
+  //     console.log("Good here");
 
-    } catch (error) {
-      const message = "Error happened";
+  //   } catch (error) {
+  //     const message = "Error happened";
 
 
-      await api.jobs.fail(job.id, { outcome: { message }});
-    }
+  //     await api.jobs.fail(job.id, { outcome: { message }});
+  //   }
   
-  }
+  // }
 }
-
 
--- src/plugins/jobs.ts
+++ src/plugins/jobs.ts
@@ -1,18 +1,32 @@
 // In here I'll want to listen for the jobs
 // Pass that to sortSubs or whatever needs it for actions
 
-// import { FlatfileEvent, FlatfileListener } from "@flatfile/listener";
-// import api, { Flatfile } from "@flatfile/api";
+import { FlatfileEvent, FlatfileListener } from "@flatfile/listener";
+import api, { Flatfile } from "@flatfile/api";
 
-import { FlatfileEvent } from "@flatfile/listener";
+// import { FlatfileEvent } from "@flatfile/listener";
 import { Flatfile } from "@flatfile/api";
 
 
 
 export type ActionSpec = {
     blueprint: Flatfile.Action;
-    handler: (event: FlatfileEvent, job: Flatfile.Job) => any;
+    // handler: (event: FlatfileEvent, job: Flatfile.Job) => any;
     // partHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
     // partsCompletedHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
-  };
+};
   
+
+
+const sortRecords = async (event: FlatfileEvent, to: string) => {
+    const { sheetId, workbookId, jobId } = event.context;
+
+    try {
+        const { data: sheets } = await api.sheets.list({ workbookId });
+        const destinationSheet = sheets.find((sheet) => sheet.config?.slug === to);
+        const { records } = await event.data;
+
+     
+    }
+
+}
