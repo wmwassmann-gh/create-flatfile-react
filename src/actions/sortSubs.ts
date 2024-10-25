3db94499faf73144d3e4935d42864d99d5d10f37
wmwassmann <wmwassmann@hotmail.com>
2024-10-25 16:05:46 -0700

    More updates

--- src/actions/sortSubs.ts
+++ src/actions/sortSubs.ts
@@ -1,15 +1,15 @@
-// import api from "@flatfile/api";
-// import { FlatfileEvent } from "@flatfile/listener";
-import { ActionSpec } from "../plugins/jobs";
+// // import api from "@flatfile/api";
+// // import { FlatfileEvent } from "@flatfile/listener";
+// import { ActionSpec } from "../plugins/jobs";
 
-// export const changeCase: ActionSpec = {
-export const countRecords: ActionSpec = {
-  blueprint: {
-    operation: "sort-subs",
-    mode: "foreground",
-    label: "Sort Subscribers",
-    description: "This will sort subscribers and non-subscribers." 
-  },
+// // export const changeCase: ActionSpec = {
+// export const countRecords: ActionSpec = {
+//   blueprint: {
+//     operation: "count-sub-records",
+//     mode: "foreground",
+//     label: "Count Subscribers",
+//     description: "This will count subscriber records." 
+//   },
 
-}
+// }
 
--- src/plugins/jobs.ts
+++ src/plugins/jobs.ts
@@ -1,54 +1,54 @@
-// In here I'll want to listen for the jobs
-// Pass that to sortSubs or whatever needs it for actions
+// // In here I'll want to listen for the jobs
+// // Pass that to sortSubs or whatever needs it for actions
 
-import { FlatfileEvent, FlatfileListener } from "@flatfile/listener";
-import api, { Flatfile } from "@flatfile/api";
-import insertSortedSubs from "../helpers/insertSortedSubs"
+// import { FlatfileEvent, FlatfileListener } from "@flatfile/listener";
+// import api, { Flatfile } from "@flatfile/api";
+// import insertSortedSubs from "../helpers/insertSortedSubs"
 
-// import { FlatfileEvent } from "@flatfile/listener";
-import { Flatfile } from "@flatfile/api";
+// // import { FlatfileEvent } from "@flatfile/listener";
+// import { Flatfile } from "@flatfile/api";
 
 
 
-export type ActionSpec = {
-    blueprint: Flatfile.Action;
-    // handler: (event: FlatfileEvent, job: Flatfile.Job) => any;
-    // partHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
-    // partsCompletedHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
-};
+// export type ActionSpec = {
+//     blueprint: Flatfile.Action;
+//     // handler: (event: FlatfileEvent, job: Flatfile.Job) => any;
+//     // partHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
+//     // partsCompletedHandler?: (event: FlatfileEvent, job: Flatfile.Job) => any;
+// };
   
 
 
-const sortRecords = async (event: FlatfileEvent, to: string) => {
-    const { sheetId, workbookId, jobId } = event.context;
+// const sortRecords = async (event: FlatfileEvent, to: string) => {
+//     const { sheetId, workbookId, jobId } = event.context;
 
-    try {
-        const { data: sheets } = await api.sheets.list({ workbookId });
-        const destinationSheet = sheets.find((sheet) => sheet.config?.slug === to);
-        const { records } = await event.data;
+//     try {
+//         const { data: sheets } = await api.sheets.list({ workbookId });
+//         const destinationSheet = sheets.find((sheet) => sheet.config?.slug === to);
+//         const { records } = await event.data;
 
 
-        if (!records) {
-            throw new Error("No records selected to move.");
-        }
+//         if (!records) {
+//             throw new Error("No records selected to move.");
+//         }
     
-        if (!destinationSheet?.id) {
-            throw new Error(`Destination sheet was not found by slug "${to}.`);
-        }
+//         if (!destinationSheet?.id) {
+//             throw new Error(`Destination sheet was not found by slug "${to}.`);
+//         }
     
-        await api.jobs.ack(jobId, { info: `Transfering ${records.length} records over to Subscribers`})
+//         await api.jobs.ack(jobId, { info: `Transfering ${records.length} records over to Subscribers`})
 
         
      
-    }
+//     }
 
-}
+// }
 
-// Study up on insertRecords concepts 
+// // Study up on insertRecords concepts 
 
 
-export function moveRecords(listener: FlatfileListener) {
-    listener.on("job:ready", { job: "sheet:sort-subs"}, async (event) => {
-        await sortRecords(event, "sheets/subs/sub")
-    });
-}
+// export function moveRecords(listener: FlatfileListener) {
+//     listener.on("job:ready", { job: "sheet:sort-subs"}, async (event) => {
+//         await sortRecords(event, "sheets/subs/sub")
+//     });
+// }
